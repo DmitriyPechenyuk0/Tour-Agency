94747b705b339944e6ca9b6410384a741da3118f
Nikita <nikitaemrih226@gmail.com>
2024-10-11 20:40:28 +0300

    create email

--- main/settings.py
+++ main/settings.py
@@ -1,6 +1,25 @@
 import flask
+from flask import Flask, render_template
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
+from flask_mail import Mail, Message
 
 project = flask.Flask(
     import_name='main',
     template_folder='templates',
 )
+
+project.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///data.db'
+
+DATABASE = flask_sqlalchemy.SQLAlchemy(app = project)
+
+MIGRATE = flask_migrate.Migrate(app = project, db = DATABASE)
+
+project.config['MAIL_SERVER'] = 'smtp.gmail.com'
+project.config['MAIL_PORT'] = 465
+project.config['MAIL_USE_SSL'] = True
+project.config['MAIL_USERNAME'] = 'touragent.practice@gmail.com'
+project.config['MAIL_PASSWORD'] = 'prwp rafv yylt xfgu'  
+project.config['MAIL_DEFAULT_SENDER'] = 'touragent.practice@gmail.com'
+
+    
--- user/views.py
+++ user/views.py
@@ -1,5 +1,14 @@
 import flask
+from .models import User
+
 
 
 def render_user():
+    if flask.request.method == 'POST':
+        print(flask.request.form)
+        user = User(
+            login = flask.request.form['login'],
+            email = flask.request.form['email'],
+            password = flask.request.form['password']
+        )
     return flask.render_template(template_name_or_list='user.html')
