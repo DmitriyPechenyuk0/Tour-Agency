0f4f2d610bab3555554f095f07efe7b20c44920c
Nikita <nikitaemrih226@gmail.com>
2024-11-11 21:31:48 +0200

    create css in registration

--- main/settings.py
+++ main/settings.py
@@ -8,6 +8,8 @@ import flask_migrate, flask_sqlalchemy
 project = flask.Flask(
     import_name='main',
     template_folder='templates',
+    static_folder='static',
+    static_url_path='/static/'
 )
 
 project.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///data.db'
