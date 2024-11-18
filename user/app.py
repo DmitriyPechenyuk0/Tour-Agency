b01cd18e1a936f12e5349253ddc7418202e1d62b
Dmitriy <dmitriypechenyuk0@gmail.com>
2024-11-18 20:20:21 +0200

    merge brances

--- user/app.py
+++ user/app.py
@@ -4,4 +4,6 @@ user = flask.Blueprint(
     name='user',
     import_name='user',
     template_folder='templates',
+    static_folder='static',
+    static_url_path='/user/'
 )
--- user/views.py
+++ user/views.py
@@ -1,6 +1,5 @@
 import flask
 from .models import User, DATABASE
-from flask_login import current_user, login_user
 
 
 def render_register():
@@ -12,15 +11,23 @@ def render_register():
         )
         DATABASE.session.add(user)
         DATABASE.session.commit()
-        return flask.redirect("/authorization")
     return flask.render_template(template_name_or_list='registration.html')
 
 def render_authorization():
+    
     if flask.request.method == "POST":
         for user in User.query.filter_by(username = flask.request.form['username']):
             if user.password == flask.request.form["password"]:
-                login_user(user)
                 return flask.redirect("/")
             
 
     return flask.render_template(template_name_or_list='authorization.html')
+
+
+
+
+
+
+
+
+
